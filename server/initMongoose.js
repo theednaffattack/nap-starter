8745b924f19559d638b9ce35c308fc3779bc3fa2
theednaffattack <eddienaff@gmail.com>
2017-09-29 14:11:01 -0700

    Fix mongoClient deprecation warning

--- server/initMongoose.js
+++ server/initMongoose.js
@@ -1,31 +1,31 @@
-const init = (mongo_url) => new Promise((resolve, reject) => {
-  const mongoose = require('mongoose')
+const init = mongo_url =>
+  new Promise((resolve, reject) => {
+    const mongoose = require("mongoose");
 
-  mongoose.Promise = global.Promise
+    mongoose.Promise = global.Promise;
 
-  mongoose.connect(mongo_url, {
-    server: {
-      auto_reconnect: true,
+    mongoose.connect(mongo_url, {
+      useMongoClient: true,
+      autoReconnect: true,
       reconnectTries: Number.MAX_VLUE,
-      reconnectInterval: 1000,
-    }
-  })
+      reconnectInterval: 1000
+    });
 
-  const connection = mongoose.connection
-  connection.on('error', (err) => {
-    // TODO : Handle error
-    if (err.message.code === 'ETIMEDOUT') {
-      debug.error(err)
-      // mongoose.createConnection(mongo_url)
-    }
+    const connection = mongoose.connection;
+    connection.on("error", err => {
+      // TODO : Handle error
+      if (err.message.code === "ETIMEDOUT") {
+        debug.error(err);
+        // mongoose.createConnection(mongo_url)
+      }
 
-    reject(err)
-  })
+      reject(err);
+    });
 
-  connection.once('open', () => {
-    debug.info(`MongoDB : ${mongo_url}`)
-    resolve(mongoose)
-  })
-})
+    connection.once("open", () => {
+      debug.info(`MongoDB : ${mongo_url}`);
+      resolve(mongoose);
+    });
+  });
 
-module.exports = init
+module.exports = init;
